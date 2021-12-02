a69e9ec31b927cd1f080c666125a4e146e4ca75e
natalia <ewewraw@gmail.com>
2021-12-02 10:22:21 +0100

    frontend finished

--- src/main/java/com/example/springboot/HomeController.java
+++ src/main/java/com/example/springboot/HomeController.java
@@ -3,6 +3,11 @@ package com.example.springboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
@@ -12,6 +17,19 @@ public class HomeController {
 	public String homePage(Model model) {
 		model.addAttribute("appName", appName);
 		return "home";
+	}
+
+	@GetMapping("/search")
+	public String homePage(Model model, HttpServletRequest request, RedirectAttributes redir, @RequestParam int number) {
+//		TODO: replace with real data
+		String owner = "some_owner";
+
+		model.addAttribute("appName", appName);
+		redir.addFlashAttribute("owner",owner);
+
+
+		String referer = request.getHeader("Referer");
+		return "redirect:"+ referer;
 	}
 
 }
