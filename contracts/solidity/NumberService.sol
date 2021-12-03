fcbd4f6f00c62cfa897c07a32eee10efff6dfed8
joel <joel.haubold@logarithmo.de>
2021-12-03 18:28:06 +0100

    Reformated data storage

--- contracts/solidity/NumberService.sol
+++ contracts/solidity/NumberService.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.3;
 contract numberService { //TODO: Currently not collecting any fees
     //Contract Owner
-    address payable contractOwner;
+    address payable numberProvider;
     uint256 ownerBalance;
 
     //User accounts
@@ -13,14 +13,14 @@ contract numberService { //TODO: Currently not collecting any fees
     mapping(address=>account) owner2account;
 
     // Number registry
-    mapping(string=>address) number2owner; //Change to offered number system
+    struct numberInformation {
+        address owner;
+        bool isBeeingRentedOrAuctionedOrListened; //If this is set true it means the number is already beeing listed/rented/auctioned. Set false when bought/returned/the auction finishes.
+    }
+    mapping(string=>numberInformation) number2numberInformation;
 
     // Marketplace
-    struct listingInformation {
-        address owner;
-        uint256 price;
-    }
-    mapping(string=>listingInformation) number2listing;
+    mapping(string=>uint256) number2listingPrice;
     string[] listedNumbers;
 
     uint costOfFreeNumber = 10; //TODO: Maybe set these in constructor?
@@ -28,21 +28,20 @@ contract numberService { //TODO: Currently not collecting any fees
     uint costOf60SecondsSubscription = 1;
 
     constructor () {
-        contractOwner = payable(msg.sender);
+        numberProvider = payable(msg.sender);
     }
 
     function receiveNumber(address receiver, string memory number) private {
-        number2owner[number] = receiver;
+        number2numberInformation[number] = numberInformation(receiver,false);
         owner2account[receiver].ownedNumbers.push(number);
     }
 
     function transferNumber(address receiver, address donor, string calldata number, uint256 pay) private {
-        number2owner[number] = receiver;
+        number2numberInformation[number] = numberInformation(receiver,false);
         string[] storage donorNumbers = owner2account[donor].ownedNumbers; // Has to be storage to reflect changes
         for (uint i = 0; i < donorNumbers.length; i++) {
             if(compareStrings(donorNumbers[i], number)){
                 donorNumbers[i] = donorNumbers[donorNumbers.length-1];
-                //delete donorNumbers[donorNumbers.length - 1];
                 donorNumbers.pop();
             }
         }
@@ -55,12 +54,12 @@ contract numberService { //TODO: Currently not collecting any fees
     }
 
     function checkOwner(string calldata number, address suspectedOwner) view external returns (bool) {
-        address numberHolder = number2owner[number];
+        address numberHolder = number2numberInformation[number].owner;
         return suspectedOwner == numberHolder;
     }
 
     function checkOwner(string calldata number) view external returns (string memory) {
-        address numberHolder = number2owner[number];
+        address numberHolder = number2numberInformation[number].owner;
         if(numberHolder == address(0x0)){
             return "Unowned";
         } else if(!compareStrings(owner2account[numberHolder].number2nickname[number],"")){
@@ -100,33 +99,33 @@ contract numberService { //TODO: Currently not collecting any fees
     }
 
     function seePriceOfListedNumber(string calldata number) view external returns (uint256) {
-        return number2listing[number].price;
+        return number2listingPrice[number];
     }
 
     function withdrawMoney(address payable sendTo) external {
         uint256 amount = owner2account[msg.sender].accountBalance;
-        if(msg.sender == contractOwner) {
+        if(msg.sender == numberProvider) {
             amount += ownerBalance;
         }
         sendTo.transfer(amount);
     }
 
-    function listNumber(string calldata number, uint256 price) external {
-        require(number2owner[number]==msg.sender, "Trying to list number that you don't own!");
-        number2listing[number] = listingInformation(msg.sender, price);
+    function listNumber(string calldata number, uint256 price) external { //Check if number is already listed/auctioned/rented
+        require(number2numberInformation[number].owner==msg.sender, "Trying to list number that you don't own!");
+        number2listingPrice[number] = price;
         listedNumbers.push(number);
     }
 
     function buyNumber(string calldata number) payable external {
-        if(number2owner[number]==address(0x0)) {
+        if(number2numberInformation[number].owner==address(0x0)) {
             require(msg.value == costOfFreeNumber, "Trying to buy a free number, with an inadequate amount of ether");
             receiveNumber(msg.sender, number);
             ownerBalance += costOfFreeNumber;
-        } else if(number2listing[number].owner != address(0x0)) {
-            require(msg.value == number2listing[number].price,"Inadequate price for listed number");
-            address donor = number2listing[number].owner;
+        } else if(number2listingPrice[number] != 0) {
+            require(msg.value == number2listingPrice[number],"Inadequate price for listed number");
+            address donor = number2numberInformation[number].owner;
             transferNumber(msg.sender, donor, number, msg.value);
-            number2listing[number] = listingInformation(address(0x0), 0);
+            number2listingPrice[number] = 0;
             for (uint i = 0; i < listedNumbers.length; i++) {
                 if(compareStrings(listedNumbers[i], number)){
                     listedNumbers[i] = listedNumbers[listedNumbers.length-1];
