ba8750d89140942f755446b13f36841dcb6c8420
joel <joel.haubold@logarithmo.de>
2021-12-06 20:26:58 +0100

    Added comments

--- contracts/solidity/NumberService.sol
+++ contracts/solidity/NumberService.sol
@@ -13,16 +13,17 @@ contract numberService {
     mapping(address=>account) owner2account;
 
     // Number registry
-    struct numberInformation { //TODO: Actually set bool
+    struct numberInformation {
         address owner;
-        bool isBeeingRentedOrAuctionedOrListed; //If this is set true it means the number is already beeing listed/rented/auctioned. Set false when bought/returned/the auction finishes.
+        bool isBeingRentedOrAuctionedOrListed; //If this is set true it means the number is already beeing listed/rented/auctioned. Set false when bought/returned/the auction finishes.
     }
     mapping(string=>numberInformation) number2numberInformation;
 
     // Marketplace
+    // Listings
     mapping(string=>uint256) number2listingPrice;
     string[] listedNumbers;
-
+    // Renting
     struct rentAvailableInformation {
         uint256 price;
         address originalOwner;
@@ -35,7 +36,7 @@ contract numberService {
     }
     mapping(string=>rentAvailableInformation) number2rentContract;
     string[] availableRentNumbers;
-
+    //Auctions
     struct auctionStateInformation {
         address highestBider;
         uint256 highestBid;
@@ -57,11 +58,13 @@ contract numberService {
         numberProvider = payable(msg.sender);
     }
 
+    //Private helper funtion to give a free number to a reveicer
     function receiveNumber(address receiver, string memory number) private {
         number2numberInformation[number] = numberInformation(receiver,false);
         owner2account[receiver].ownedNumbers.push(number);
     }
 
+    //Private helper funtion to transfer a number and mark the pay for the donor
     function transferNumber(address receiver, address donor, string calldata number, uint256 pay) internal {
         number2numberInformation[number] = numberInformation(receiver,false);
         string[] storage donorNumbers = owner2account[donor].ownedNumbers; // Has to be storage to reflect changes
@@ -75,27 +78,13 @@ contract numberService {
         owner2account[donor].accountBalance += pay;
     }
 
+    //Private helper function to compare two strings for equality
     function compareStrings(string memory s1, string memory s2) internal pure returns (bool){
         return keccak256(bytes(s1)) == keccak256(bytes(s2));
     }
 
-    function checkOwner(string calldata number, address suspectedOwner) view external returns (bool) {
-        address numberHolder = number2numberInformation[number].owner;
-        return suspectedOwner == numberHolder;
-    }
-
-    function checkOwner(string calldata number) view external returns (string memory) {
-        address numberHolder = number2numberInformation[number].owner;
-        if(numberHolder == address(0x0)){
-            return "Unowned";
-        } else if(!compareStrings(owner2account[numberHolder].number2nickname[number],"")){
-            return owner2account[numberHolder].number2nickname[number];
-        } else {
-            return toString(abi.encodePacked(numberHolder));
-        }
-    }
-
-    function toString(bytes memory data) internal pure returns(string memory) { //Help function for check owner
+    // Private helper function to translate bytes into a readable string
+    function toString(bytes memory data) internal pure returns(string memory) {
         bytes memory alphabet = "0123456789abcdef"; //Hexa alphabet to translate bits to string
 
         bytes memory str = new bytes(2 + data.length * 2);
@@ -108,22 +97,45 @@ contract numberService {
         return string(str);
     }
 
+    //See if the suspected owner is the owner of the number
+    function checkOwner(string calldata number, address suspectedOwner) view external returns (bool) {
+        address numberHolder = number2numberInformation[number].owner;
+        return suspectedOwner == numberHolder;
+    }
+
+    //Return the Owner of the given number
+    function checkOwner(string calldata number) view external returns (string memory) {
+        address numberHolder = number2numberInformation[number].owner;
+        if(numberHolder == address(0x0)){
+            return "Unowned";
+        } else if(!compareStrings(owner2account[numberHolder].number2nickname[number],"")){
+            return owner2account[numberHolder].number2nickname[number];
+        } else {
+            return toString(abi.encodePacked(numberHolder));
+        }
+    }
+
+    //Return all owned numbers of the caller
     function seeOwnedNumbers() view external returns (string[] memory) {
         return owner2account[msg.sender].ownedNumbers;
     }
 
+    //Return the balance of the caller
     function seeBalance() view external returns (uint256) {
         return owner2account[msg.sender].accountBalance;
     }
 
+    //See all numbers listed for potential buyers
     function seeListedNumbers() view external returns (string[] memory) {
         return listedNumbers;
     }
 
+    //See the price of a listed number
     function seePriceOfListedNumber(string calldata number) view external returns (uint256) {
         return number2listingPrice[number];
     }
 
+    //Transfers to the caller all the money he earned
     function withdrawMoney(address payable sendTo) external {
         uint256 amount = owner2account[msg.sender].accountBalance;
         if(msg.sender == numberProvider) {
@@ -132,15 +144,17 @@ contract numberService {
         sendTo.transfer(amount);
     }
 
+    //List a number of the caller for the given price for a potential buyer
     function listNumber(string calldata number, uint256 price) external { //Check if number is already listed/auctioned/rented
         require(price>0,"Rent price has to be higher than 0");
         require(number2numberInformation[number].owner==msg.sender, "Trying to list number that you don't own!");
-        require(number2numberInformation[number].isBeeingRentedOrAuctionedOrListed == false, "Number is not available for listing");
-        number2numberInformation[number].isBeeingRentedOrAuctionedOrListed = true;
+        require(number2numberInformation[number].isBeingRentedOrAuctionedOrListed == false, "Number is not available for listing");
+        number2numberInformation[number].isBeingRentedOrAuctionedOrListed = true;
         number2listingPrice[number] = price;
         listedNumbers.push(number);
     }
 
+    //Buy a listed number
     function buyNumber(string calldata number) payable external {
         if(number2numberInformation[number].owner==address(0x0)) {
             require(msg.value == costOfFreeNumber, "Trying to buy a free number, with an inadequate amount of ether");
@@ -157,28 +171,29 @@ contract numberService {
                     listedNumbers.pop();
                 }
             }
-            number2numberInformation[number].isBeeingRentedOrAuctionedOrListed = false;
+            number2numberInformation[number].isBeingRentedOrAuctionedOrListed = false;
         } else {
             require(false, "Number is neither available nor listed by it's owner");
         }
 
     }
 
+    //Buy a nickname for a number to be displayed if you own that number
     function buyNickname(string calldata nickname, string calldata number) payable external {
         require(msg.value == costOfNickname, "Inadequate amount of ether for nickname");
         owner2account[msg.sender].number2nickname[number] = nickname;
     }
 
     //Functions for renting:
-
+    //Rent a number that was marked by its owner as rentable for the given number of seconds. Price depends on the rent duration as specifie by the owners
     function rentNumber(string calldata number, uint256 nmbrSeconds) payable external {
         require(number2rentContract[number].price!=0, "This number is not available to rent");
-        require(number2rentContract[number].price+costOfReturnDelay==msg.value, "Inadequate price for renting this number");
+        require((number2rentContract[number].price)*nmbrSeconds+costOfReturnDelay==msg.value, "Inadequate price for renting this number");
         require(number2rentContract[number].currentActiveRent.renter == address(0x0), "This number is already beeing rented");
         uint256 endTimestamp = nmbrSeconds + block.timestamp;
         require(number2rentContract[number].endTimestamp > endTimestamp, "Trying to rent number for longer than its availability");
         number2rentContract[number].currentActiveRent = rentActiveInformation(msg.sender, endTimestamp);
-        transferNumber(msg.sender, number2rentContract[number].originalOwner, number, msg.value);
+        transferNumber(msg.sender, number2rentContract[number].originalOwner, number, msg.value-costOfReturnDelay);
         for (uint i = 0; i < availableRentNumbers.length; i++) {
             if(compareStrings(availableRentNumbers[i], number)){
                 availableRentNumbers[i] = availableRentNumbers[availableRentNumbers.length-1];
@@ -187,15 +202,18 @@ contract numberService {
         }
     }
 
+    //Makes a number owned by the caller available to rent for the given number of seconds. Renters will be charged the price per second.
+    //You have to prepay a fee that is kept if you don't return the rented number on time with rentEndInstance.
     function rentMakeNumberAvailable(string calldata number, uint256 price, uint256 nmbrSeconds) external {
         require(price>0,"Rent price has to be higher than 0");
         require(number2numberInformation[number].owner == msg.sender, "Trying to rent out a number that you don't own");
-        require(number2numberInformation[number].isBeeingRentedOrAuctionedOrListed == false, "Number is not available for rent");
-        number2numberInformation[number].isBeeingRentedOrAuctionedOrListed = true;
+        require(number2numberInformation[number].isBeingRentedOrAuctionedOrListed == false, "Number is not available for rent");
+        number2numberInformation[number].isBeingRentedOrAuctionedOrListed = true;
         availableRentNumbers.push(number);
         number2rentContract[number] = rentAvailableInformation(price, msg.sender, nmbrSeconds + block.timestamp, rentActiveInformation(address(0x0), 0));
     }
 
+    //End the renting of a number if the rent duration elapsed. If returned to late the prepayed fee will be kept
     function rentEndInstance(string calldata number) external {
         require(number2rentContract[number].currentActiveRent.endTimestamp < block.timestamp, "Rent session hasn't expired yet");
         uint256 feePayback = 0;
@@ -207,6 +225,7 @@ contract numberService {
         availableRentNumbers.push(number);
     }
 
+    //End the number being available for rent if the prespecified duration elapsed
     function rentEndAvailability(string calldata number) external {
         require(number2rentContract[number].originalOwner != address(0x0), "Number is not listed as rentable");
         require(number2rentContract[number].endTimestamp < block.timestamp, "Rent duration hasn't expired yet");
@@ -219,14 +238,16 @@ contract numberService {
                 availableRentNumbers.pop();
             }
         }
-        number2numberInformation[number].isBeeingRentedOrAuctionedOrListed = false;
+        number2numberInformation[number].isBeingRentedOrAuctionedOrListed = false;
         number2rentContract[number] = rentAvailableInformation(0, address(0x0), 0, rentActiveInformation(address(0x0), 0));
     }
 
+    //Returns all numbers available to rent
     function rentSeeAvailableNumbers() view external returns (string[] memory) {
         return availableRentNumbers;
     }
 
+    //Returns information on a number that can be rent
     function rentGetInformationOnNumber(string calldata number) view external returns (uint256, uint256) {
         return (number2rentContract[number].price, number2rentContract[number].endTimestamp);
     }
@@ -235,8 +256,8 @@ contract numberService {
     //Start auction for number
     function auctionStart(string calldata number, uint256 nmbrSecondsDuration) external {
         require(number2numberInformation[number].owner == msg.sender, "Trying to rent out a number that you don't own");
-        require(number2numberInformation[number].isBeeingRentedOrAuctionedOrListed == false, "Number is not available for rent");
-        number2numberInformation[number].isBeeingRentedOrAuctionedOrListed = true;
+        require(number2numberInformation[number].isBeingRentedOrAuctionedOrListed == false, "Number is not available for rent");
+        number2numberInformation[number].isBeingRentedOrAuctionedOrListed = true;
         numbersBeingAuctioned.push(number);
         number2auctionState[number] = auctionStateInformation(address(0x0), 0, block.timestamp +  nmbrSecondsDuration);
     }
@@ -261,7 +282,7 @@ contract numberService {
                 numbersBeingAuctioned.pop();
             }
         }
-        number2numberInformation[number].isBeeingRentedOrAuctionedOrListed = false;
+        number2numberInformation[number].isBeingRentedOrAuctionedOrListed = false;
         number2auctionState[number] = auctionStateInformation(address(0x0),0,0);
     }
 
