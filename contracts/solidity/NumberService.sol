c44ba267c5236d927daa0068921d56fba963fc0e
joel <joel.haubold@logarithmo.de>
2021-12-06 04:46:42 +0100

    Implemented number renting in theory

--- contracts/solidity/NumberService.sol
+++ contracts/solidity/NumberService.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.8.3;
-contract numberService { //TODO: Currently not collecting any fees
+contract numberService {
     //Contract Owner
     address payable numberProvider;
     uint256 ownerBalance;
@@ -13,19 +13,36 @@ contract numberService { //TODO: Currently not collecting any fees
     mapping(address=>account) owner2account;
 
     // Number registry
-    struct numberInformation {
+    struct numberInformation { //TODO: Actually set bool
         address owner;
-        bool isBeeingRentedOrAuctionedOrListened; //If this is set true it means the number is already beeing listed/rented/auctioned. Set false when bought/returned/the auction finishes.
+        bool isBeeingRentedOrAuctionedOrListed; //If this is set true it means the number is already beeing listed/rented/auctioned. Set false when bought/returned/the auction finishes.
     }
     mapping(string=>numberInformation) number2numberInformation;
 
     // Marketplace
     mapping(string=>uint256) number2listingPrice;
     string[] listedNumbers;
+    struct rentAvailableInformation {
+        uint256 price;
+        address originalOwner;
+        uint256 endTimestamp;
+        rentActiveInformation currentActiveRent;
+    }
+    struct rentActiveInformation {
+        address renter;
+        uint256 endTimestamp;
+    }
+    mapping(string=>rentAvailableInformation) number2rentContract;
+    string[] availableRentNumbers;
 
-    uint costOfFreeNumber = 10; //TODO: Maybe set these in constructor?
-    uint costOfNickname = 100;
-    uint costOf60SecondsSubscription = 1;
+    //Costs
+    uint costOfFreeNumber = 10 gwei;
+    uint costOfNickname = 100 gwei;
+    //uint costOf60SecondsSubscription = 1 gwei;
+    uint costOfReturnDelay = 1 gwei;
+
+    //Timeframes
+    uint permittedRentReturnDelay = 60 seconds;
 
     constructor () {
         numberProvider = payable(msg.sender);
@@ -36,7 +53,7 @@ contract numberService { //TODO: Currently not collecting any fees
         owner2account[receiver].ownedNumbers.push(number);
     }
 
-    function transferNumber(address receiver, address donor, string calldata number, uint256 pay) private {
+    function transferNumber(address receiver, address donor, string calldata number, uint256 pay) internal {
         number2numberInformation[number] = numberInformation(receiver,false);
         string[] storage donorNumbers = owner2account[donor].ownedNumbers; // Has to be storage to reflect changes
         for (uint i = 0; i < donorNumbers.length; i++) {
@@ -82,10 +99,6 @@ contract numberService { //TODO: Currently not collecting any fees
         return string(str);
     }
 
-    function seeTransactions() view external { //TODO: Blockchain already does this for us?
-
-    }
-
     function seeOwnedNumbers() view external returns (string[] memory) {
         return owner2account[msg.sender].ownedNumbers;
     }
@@ -111,7 +124,10 @@ contract numberService { //TODO: Currently not collecting any fees
     }
 
     function listNumber(string calldata number, uint256 price) external { //Check if number is already listed/auctioned/rented
+        require(price>0,"Rent price has to be higher than 0");
         require(number2numberInformation[number].owner==msg.sender, "Trying to list number that you don't own!");
+        require(number2numberInformation[number].isBeeingRentedOrAuctionedOrListed == false, "Number is not available for listing");
+        number2numberInformation[number].isBeeingRentedOrAuctionedOrListed = true;
         number2listingPrice[number] = price;
         listedNumbers.push(number);
     }
@@ -132,6 +148,7 @@ contract numberService { //TODO: Currently not collecting any fees
                     listedNumbers.pop();
                 }
             }
+            number2numberInformation[number].isBeeingRentedOrAuctionedOrListed = false;
         } else {
             require(false, "Number is neither available nor listed by it's owner");
         }
@@ -143,37 +160,93 @@ contract numberService { //TODO: Currently not collecting any fees
         owner2account[msg.sender].number2nickname[number] = nickname;
     }
 
-    function startSubscription(uint256 nmbrSeconds) payable external {
+    function startRent(string calldata number, uint256 nmbrSeconds) payable external {
+        require(number2rentContract[number].price!=0, "This number is not available to rent");
+        require(number2rentContract[number].price==msg.value+costOfReturnDelay, "Inadequate price for renting this number");
+        require(number2rentContract[number].currentActiveRent.renter != address(0x0), "This number is not available to rent");
+        uint256 endTimestamp = nmbrSeconds + block.timestamp;
+        require(number2rentContract[number].endTimestamp > endTimestamp, "Trying to rent number for longer than its availability");
+        number2rentContract[number].currentActiveRent = rentActiveInformation(msg.sender, endTimestamp);
+        transferNumber(msg.sender, number2rentContract[number].originalOwner, number, msg.value);
+        for (uint i = 0; i < availableRentNumbers.length; i++) {
+            if(compareStrings(availableRentNumbers[i], number)){
+                availableRentNumbers[i] = availableRentNumbers[availableRentNumbers.length-1];
+                availableRentNumbers.pop();
+            }
+        }
+    }
 
+    function allowRenting(string calldata number, uint256 price, uint256 nmbrSeconds) external {
+        require(price>0,"Rent price has to be higher than 0");
+        require(number2numberInformation[number].isBeeingRentedOrAuctionedOrListed == false, "Number is not available for rent");
+        number2numberInformation[number].isBeeingRentedOrAuctionedOrListed = true;
+        availableRentNumbers.push(number);
+        number2rentContract[number] = rentAvailableInformation(price, msg.sender, nmbrSeconds + block.timestamp, rentActiveInformation(address(0x0), 0));
+    }
+
+    function reclaimRentedNumber(string calldata number) external {
+        require(number2rentContract[number].currentActiveRent.endTimestamp < block.timestamp, "Rent session hasn't expired yet");
+        uint256 fee = 0;
+        if(number2rentContract[number].endTimestamp + permittedRentReturnDelay < block.timestamp) {
+            fee = costOfReturnDelay;
+        }
+        transferNumber(number2rentContract[number].originalOwner, number2rentContract[number].currentActiveRent.renter, number, fee);
+        number2rentContract[number].currentActiveRent = rentActiveInformation(address(0x0), 0);
+        availableRentNumbers.push(number);
+    }
+
+    function confirmRentEnding(string calldata number) external {
+        require(number2rentContract[number].endTimestamp < block.timestamp, "Rent duration hasn't expired yet");
+        if(number2rentContract[number].currentActiveRent.renter != address(0x0)) {
+            this.reclaimRentedNumber(number);
+        }
+        number2numberInformation[number].isBeeingRentedOrAuctionedOrListed = false;
+        number2rentContract[number] = rentAvailableInformation(0, address(0x0), 0, rentActiveInformation(address(0x0), 0));
+    }
+
+    function seeAvailableRentNumbers() view external returns (string[] memory) {
+        return availableRentNumbers;
+    }
+
+    function seeInformationOfAvailableRentNumber(string calldata number) view external returns (uint256, uint256) {
+        return (number2rentContract[number].price, number2rentContract[number].endTimestamp);
     }
 
     //TODO: Closed off transaction
 
 }
 
-contract mortal {
-    address payable owner;
-    constructor() { owner = payable(msg.sender); }
-    function kill() public { if (msg.sender == owner) selfdestruct(owner); }
-}
-
-contract numberSubscription is mortal { //TODO: How to return to base?
-    struct reservation {
-        address reserver;
-        uint endTimestamp;
-    }
-    numberService service;
-    mapping(uint=>reservation) startTimestamp2Reservation;
-    uint forseableEndingTimestamp;
-
-    constructor(uint256 nmbrSeconds, string memory originalOwner) mortal() {
-        forseableEndingTimestamp = block.timestamp + nmbrSeconds;
-        startTimestamp2Reservation[block.timestamp] = reservation(msg.sender, forseableEndingTimestamp);
-    }
-
-    function claim() internal {//Transfer ownership if timestamps allow
-
-
-    }
-
-}
+//contract mortal { <--- Doesn't work because of method visibility
+//    address payable contractOwner;
+//    constructor() { contractOwner = payable(msg.sender); }
+//    function kill() internal { if (msg.sender == contractOwner) selfdestruct(contractOwner); }
+//}
+//
+//contract numberRenting is mortal {
+//    address originalOwner;
+//    uint256 rentingAvailabilityEndTimestamp; //End timestamp for this contract
+//    uint256 rentingPeriodEndTimestamp; //End timestamp for the current rent
+//    numberService service;
+//    string rentingNumber;
+//    uint256 rentPrice;
+//
+//    constructor(uint256 _nmbrSeconds, string memory _originalOwner, string _rentingNumber, uint256 _rentPrice) mortal() {
+//        endTimestamp = block.timestamp + _nmbrSeconds;
+//        originalOwner = _originalOwner;
+//        rentingNumber = _rentingNumber;
+//        rentPrice = _rentPrice;
+//    }
+//
+//    function rentOutNumber() {
+//        service.transferNumber(originalOwner, contractOwner, subscribedNumber, 0);
+//    }
+//
+//    function checkEnding() internal returns(uint256) {//Transfer ownership if timestamps allow
+//        if(endTimestamp<block.timestamp) {
+//            service.transferNumber(originalOwner, contractOwner, subscribedNumber, 0);
+//            kill();
+//        }
+//        return endTimestamp;
+//    }
+//
+//}
