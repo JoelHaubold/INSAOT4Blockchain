719b7c408a036a95c5b0259488fb1f6ac37ded69
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2021-12-07 23:07:51 +0100

    Reverted

--- src/main/java/com/example/springboot/Singleton.java
+++ src/main/java/com/example/springboot/Singleton.java
@@ -9,24 +9,23 @@ import org.web3j.tx.gas.DefaultGasProvider;
 public final class Singleton {
     private static Singleton instance;
 
-    private static final String CONTRACT_ADDRESS = "0xbe78c7713dfe64ba3ea01eccd7b3a48d04b81b78"; // TODO: change accordingly
+    private static final String CONTRACT_ADDRESS = "0xda2db3aa26578798502986c63154b46f01a6a7c0"; // TODO: change accordingly
 
     private static final String SOURCE = System.getenv("SOURCE");
     private static final String SOURCE_PASSWORD = System.getenv("SOURCE_PASSWORD");
 
-    private NumberService contract;
+    private final NumberService contract;
+    private final Credentials credentials = WalletUtils.loadCredentials(
+            SOURCE_PASSWORD,
+            SOURCE
+    );
     private final Web3j web = Web3j.build(new HttpService());
 
     private Singleton() throws Exception {
-        Credentials credentials = WalletUtils.loadCredentials(
-                SOURCE_PASSWORD,
-                SOURCE
-        );
-
         this.contract = NumberService.load(
                 CONTRACT_ADDRESS,
                 this.getWeb(),
-                credentials,
+                this.getCredentials(),
                 new DefaultGasProvider()
         );
     }
@@ -44,5 +43,9 @@ public final class Singleton {
             instance = new Singleton();
         }
         return instance;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
     }
 }
