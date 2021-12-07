1ba6bc588799071975947790da2c5f6b57e17ede
natalia <ewewraw@gmail.com>
2021-12-07 21:53:08 +0100

    some fixes for auction

--- src/main/java/com/example/springboot/AuctionController.java
+++ src/main/java/com/example/springboot/AuctionController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.web3j.tuples.generated.Tuple2;
 
+import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -76,21 +78,17 @@ public class AuctionController {
 			myPhoneNumbers.addAll((List<String>) result);
 			myPhoneNumbers.remove(0);
 		}
-		AuctionItem[] myBids = {
-				new AuctionItem(1112223333, 10, "address1"),
-				new AuctionItem(1112223444, 10, "address2"),
-				new AuctionItem(1112225555, 10, "address3"),
-				new AuctionItem(1112226666, 12, "myAddress"),
-		};
 
-		AuctionItem[] auctionItems = {
-				new AuctionItem(999888777, 10, "address1"),
-				new AuctionItem(888777666, 10, "address2"),
-				new AuctionItem(777666555, 10, "address3"),
-		};
+		List<AuctionItem> auctionItems = new ArrayList<>();
+		List available = contract.auctionSeeAvailable().send();
+		for (Object number : available) {
+			String numberAsString = number.toString();
+			// First value is highest bid, second is timestamp
+			Tuple2<BigInteger, BigInteger> value = contract.auctionGetInformation(numberAsString).send();
+			auctionItems.add(new AuctionItem(numberAsString, value.getValue1(), value.getValue2()));
+		}
 
 		model.addAttribute("myPhoneNumbers", myPhoneNumbers);
-		model.addAttribute("myBids", myBids);
 		model.addAttribute("auctionItems", auctionItems);
 		model.addAttribute("myAddress", "myAddress");
 
@@ -100,16 +98,16 @@ public class AuctionController {
 
 	// TODO: remove when have real data
 	private class AuctionItem {
-		public int phoneNumber;
-		public int highestBid;
-		public LocalDateTime deadline;
+		public String phoneNumber;
+		public BigInteger highestBid;
+		public BigInteger deadline;
 		public String highestBidUserAddress;
 		public List<String> competingUserAdresses = new ArrayList<>();
 
-		public AuctionItem(int phoneNumber, int highestBid, String highestBidUserAddress) {
+		public AuctionItem(String phoneNumber, BigInteger highestBid, BigInteger deadline) {
 			this.phoneNumber = phoneNumber;
 			this.highestBid = highestBid;
-			this.deadline = LocalDateTime.now().plusHours((int) ((Math.random() * (30)) + 3)); // some random deadline
+			this.deadline = deadline; // some random deadline
 			this.highestBidUserAddress = highestBidUserAddress;
 			this.competingUserAdresses = Arrays.asList("address1", "address2", "address3", "myAddress"); // to track users to whom we will display their bids
 		}
